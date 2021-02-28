bf47557a528d898d1e210fec0ad64a72fbc23bf4
dayatwork <dayatwork095@gmail.com>
2021-02-28 21:18:56 +0700

    edit db connection setting

--- db/index.js
+++ db/index.js
@@ -1,6 +1,16 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false,
+    ca: process.env.CACERT,
+  },
+});
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
